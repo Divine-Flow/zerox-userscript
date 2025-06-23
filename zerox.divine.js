4f9596554596493497645296577a63c45e323589
Divine <divineismylife@gmail.com>
2025-06-23 20:50:02 +0800

    Update zerox.divine.js

--- zerox.divine.js
+++ zerox.divine.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ZEROX Advanced Bypasser [Modern UI - Redirect Only]
 // @namespace    http://tampermonkey.net/
-// @version      1.0.8
+// @version      1.0.9
 // @description  💡 Built for speed. Designed for users. Powered by Divine.
 // @author       Divine Reinhard Micheal
 // @updateURL    https://raw.githubusercontent.com/Divine-Flow/zerox-userscript/main/zerox.divine.js
